086fb23a694259978881e4fdf344209307873fa1
Ge3kXm <7624619@qq.com>
2016-08-02 23:29:14 +0800

    添加导航栏

---
+++ MXInteraciton/Main/MXViewController.h
@@ -0,0 +1,13 @@
+//
+//  MXViewController.h
+//  MXInteraciton
+//
+//  Created by maRk'sTheme on 16/8/2.
+//  Copyright © 2016年 maRk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MXViewController : UINavigationController
+
+@end
